f97633c5b625b4abf987ab4868cf797162477616
josh_carrizo <sistemas@VRE_NB_CAP1804>
2020-08-04 08:48:56 -0400

    [TAREA] parte-2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="logo" class="logo" src="https://miro.medium.com/max/1125/1*wotzQboYWAfaj-7bvGNIkQ.png">
     <Datos />
   </div>
 </template>
@@ -24,5 +24,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.logo{
+  width: 30rem;
+  height: 50%;
 }
 </style>
--- src/components/Datos.vue
+++ src/components/Datos.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="cuadro">
-
-      <h3>{{nombre_user}}</h3>
-        <ul> 
-            <li>Public Repos :{{public_repos}}</li>
-            <li>Number of followers:  {{followers}}</li>
-            <li>Number Following: {{following}}</li>
-            <li>Public Gist: {{public_gist}}</li>
-            <li>Total Score: {{total_score}}</li>
+<div class="row">
+    <div class="col-sm"></div>
+    <div class="card col-sm bg-dark" style="width: 18rem;">
+        <input v-model="user" type="String" placeholder="Selecciona Githuber"/>
+        <button class="btn btn-primary btn-jittery" @click="cargar_datos">Cargar Datos del Githuber</button>
+        <img :src="avatar" class="card-img-top" alt="...">            
+        <div class="card-body">
+            <h5 class="card-title text-light">{{user}}</h5>
+             <p class="card-text text-light">
+            Esta es la información de {{user}}
+            </p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Public Repos :{{public_repos}}</li>
+            <li class="list-group-item">Number of followers:  {{followers}}</li>
+            <li class="list-group-item">Number Following: {{following}}</li>
+            <li class="list-group-item">Public Gist: {{public_gist}}</li>
+            <li class="list-group-item"><li>Total Score: {{total_score}}</li>
         </ul>
-  </div>
+    </div>
+    <div class="col-sm"></div>
+</div>
 </template>
 
 <script>
@@ -17,7 +28,7 @@ export default {
     name: "Datos",
     data(){
         return {
-            nombre_user: "",
+            user:"josh-carrizo",
             public_repos: 0,
             followers: 0,
             following: 0,
@@ -25,26 +36,95 @@ export default {
             total_score: 0
         }
     },
-   mounted() {
-    // axios en forma de promesas (ES6)
-    this.axios.get('https://api.github.com/users/josh-carrizo')
-    .then((datos) => {
-      const user = datos.data;
+    methods: {
+        cargar_datos(){
+            this.axios.get(`https://api.github.com/users/${this.user}`)
+            .then((datos) => {
+            const user_github = datos.data;
+            this.avatar = user_github.avatar_url;
+            this.public_repos= user_github.public_repos;
+            this.followers = user_github.followers;
+            this.following = user_github.following;
+            this.public_gist = user_github.public_gists;
+            this.total_score = this.public_repos + this.public_gist + this.followers + this.following;
+            })
+            .catch((error) => {
+            alert(error);
+            });
+        }
+    },
+      mounted() {
+        // axios en forma de promesas (ES6)
+        this.cargar_datos() 
+    }
 
-      this.nombre_user = user.login;
-      this.public_repos= user.public_repos;
-      this.followers = user.followers;
-      this.following = user.following;
-      this.public_gist = user.public_gists;
-      this.total_score = this.public_repos + this.public_gist + this.followers + this.following;
-    })
-    .catch((error) => {
-      alert(error);
-    });
-
-  }
 }
 
-
 </script>
+<style scoped>
+p div il ul {
+font-family: 'Vollkorn', serif;
 
+}
+.card-img-top{
+    width: 3rem;
+    height: 3rem;
+    display: inline-block;
+    margin-left: 13.2rem;
+    }
+    .btn {
+    margin: 1rem;
+    opacity:80%;
+    }
+    .btn-jittery {
+    animation: jittery 4s infinite;
+    }
+    @keyframes jittery {
+    5%, 50% {
+    transform: scale(1);
+    }
+    10% {
+    transform: scale(0.9);
+    }
+    15% {
+    transform: scale(1.15);
+    }
+    20% {
+    transform: scale(1.15) rotate(-5deg);
+    }
+    25% {
+    transform: scale(1.15) rotate(5deg);
+    }
+    30% {
+    transform: scale(1.15) rotate(-3deg);
+    }
+    35% {
+    transform: scale(1.15) rotate(2deg);
+    }
+    40% {
+    transform: scale(1.15) rotate(0);
+    }
+    }
+    @keyframes jelly {
+    25% {
+    transform: scale(0.9, 1.1);
+    }
+    50% {
+    transform: scale(1.1, 0.9);
+    }
+    75% {
+    transform: scale(0.95, 1.05);
+    }
+    }
+    @keyframes pulse {
+    from {
+    box-shadow: 0 0 0 0 var(--btn-bg);
+    }
+    }
+    @keyframes move-left {
+    to {
+    transform: translateX(-100%);
+    }
+    }
+
+</style>
